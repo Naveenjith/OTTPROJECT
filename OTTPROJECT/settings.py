89e8c126409cc5d8e1da9648e432c693d42690f0
Naveenjith <navinjith555@gmail.com>
2025-04-23 15:26:57 +0530

    added *

--- OTTPROJECT/settings.py
+++ OTTPROJECT/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-40)xjhkv5*$u+*!zc9fhzb7uu9$#hm-h=_jk5o($!t-2d7gf&r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
